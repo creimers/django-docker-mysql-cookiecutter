7ef88b7bfc4d3501876a06983567f6ab72790dab
creimers <christoph@superservice-international.com>
2016-04-27 17:08:33 +0200

    improve checker

--- bin/check_availability.py
+++ bin/check_availability.py
@@ -2,19 +2,31 @@ import requests
 
 def check(pin):
     try:
-        import RPi.GPIO as GPIO
-        import time
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin, GPIO.OUT)
-
         response = requests.get('http://localhost:8000')
-        if response.status_code == 200:
-            GPIO.output(pin, True)
-        else:
-            GPIO.output(pin, False)
-
     except:
-        pass
+        response = None
+    
+    if response and repsonse.status_code == 200:
+        try:
+            import RPi.GPIO as GPIO
+            import time
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(pin, GPIO.OUT)
+
+            GPIO.output(pin, True)
+        except:
+            pass
+
+    else:
+        try:
+            import RPi.GPIO as GPIO
+            import time
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(pin, GPIO.OUT)
+
+            GPIO.output(pin, False)
+        except:
+            pass
 
 if __name__ == '__main__':
     check(18)
